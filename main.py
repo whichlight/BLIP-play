394a9d075c07e44c3010486c105c17ff5fdb0f1e
whichlight <whichlight@gmail.com>
2023-10-12 02:07:32 -0700

    adding chat like history

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, render_template, session
 from werkzeug.utils import secure_filename
+from flask_session import Session
 import replicate
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'  # replace 'secret' with a real secret key
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    session.clear()
+    chat = session.get('chat', [])
+    print(chat)
+    return render_template('index.html', chat=chat, filename=session.get('filename'))
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -19,23 +25,26 @@ def upload_file():
         filename = secure_filename(file.filename)
         file.save('static/images/' + filename)
         session['filename'] = filename
-        return render_template('index.html', filename=filename)
+        return render_template('index.html', filename=filename, chat=[])
 
 @app.route('/submit', methods=['POST'])
-def submit():    
+def submit():
+    chat = session.get('chat', [])
     filename = session.get('filename')
     if filename is None:
         return 'No image uploaded'
-    question = request.form.get('question') 
+    question = request.form.get('question')
     print(question)
     with open('static/images/' + filename, "rb") as image_file:
         output = replicate.run(
             'salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746',
             input={'image': image_file, 
-                   'task' : "visual_question_answering",
+                   'task': "visual_question_answering", 
                    'question': question}
         )
-    return render_template('index.html', filename=filename, output=output)
+    chat.append({'question': question, 'answer': output})
+    session['chat'] = chat
+    return render_template('index.html', filename=filename, chat=chat)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
